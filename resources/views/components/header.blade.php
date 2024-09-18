aec3fc641c78b90ac4d43efb33a2565ffe3754ec
GibranAja <mgibranarraffi@gmail.com>
2024-09-19 05:59:27 +0700

    feat: add looping for take the data

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,5 @@
+@props(['konten']);
+
 <header class="masthead bg-primary text-white text-center">
     <div class="container d-flex align-items-center flex-column">
         <!-- Masthead Avatar Image-->
@@ -11,6 +13,9 @@
             <div class="divider-custom-line"></div>
         </div>
         <!-- Masthead Subheading-->
-        <p class="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
+        
+        @foreach ($konten as $item)
+            <p class="masthead-subheading font-weight-light mb-0">{{ $item->konten }}</p>
+        @endforeach
     </div>
 </header>
